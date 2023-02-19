0556858448fdf690ef5346ea253670d5e47714b6
boreesych <bartenev@outlook.com>
2023-02-19 14:08:05 +0000

    fix models

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -5,13 +5,13 @@ from core.models import PublishedModel
 
 class Category(PublishedModel):
     title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
 
 
 class Topping(PublishedModel):
     title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
 
 
 class Wrapper(PublishedModel):
